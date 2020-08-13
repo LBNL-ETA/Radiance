77a85e3b65b6193d7f35a92d7ba8abd184454f99
MingboPeng <mingo1214@gmail.com>
2020-08-13 00:31:16 -0400

    feat(Perl): compile perl scripts

---
+++ merge.py
@@ -0,0 +1,20 @@
+import os
+import shutil
+
+
+def mergefolders(root_src_dir, root_dst_dir):
+    for src_dir, dirs, files in os.walk(root_src_dir):
+        dst_dir = src_dir.replace(root_src_dir, root_dst_dir, 1)
+        if not os.path.exists(dst_dir):
+            os.makedirs(dst_dir)
+        for file_ in files:
+            src_file = os.path.join(src_dir, file_)
+            dst_file = os.path.join(dst_dir, file_)
+            if os.path.exists(dst_file):
+                os.remove(dst_file)
+            shutil.copy(src_file, dst_dir)
+
+
+working_dir = os.getcwd()
+mergefolders(f'{working_dir}\\PAR', 'c:\\strawberry\\perl')
+
