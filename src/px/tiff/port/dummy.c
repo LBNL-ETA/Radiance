f8d03fe0d19a447889d65d3671117afed848b0e3
Mingbo Peng <mingo1214@gmail.com>
2021-05-18 22:39:26 -0400

    fix(auxiliary): update auxiliary files

---
+++ src/px/tiff/port/dummy.c
@@ -0,0 +1,12 @@
+/* $Id: dummy.c,v 1.1 2004/06/07 19:34:58 dron Exp $ */
+
+/*
+ * Dummy function, just to be ensure that the library always will be created.
+ */
+
+void
+libport_dummy_function()
+{
+        return;
+}
+
