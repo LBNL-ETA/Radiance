97bf110badac7b90b019367bc99b0eb99b1f1f7c
Mingbo Peng <mingo1214@gmail.com>
2025-07-24 13:23:24 -0400

    chore(ci): improve cvsimport.sh with comments

--- cvsimport.sh
+++ cvsimport.sh
@@ -1,2 +1,20 @@
-git cvsimport -o master -v -d :pserver:anonymous@radiance-online.org:/home/cvsd/radiance -A authors.txt ray
+#!/bin/bash
+#
+# This script imports the 'ray' module from the public Radiance CVS repository
+# into a new Git repository.
+#
+# It requires an 'authors.txt' file in the same directory to map
+# CVS usernames to Git author names and emails.
+
 set -e
+
+echo "Starting CVS import of Radiance 'ray' module..."
+
+git cvsimport -k \
+    -v \
+    -o master \
+    -d :pserver:anonymous@radiance-online.org:/home/cvsd/radiance \
+    -A authors.txt \
+    ray
+
+echo "CVS import completed successfully."
